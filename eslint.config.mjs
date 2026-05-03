c78bee3e42f28b2f02567fb2e87f16d4d7cad6b4
jasper - Laptop <pianoart@zhoujingren.com>
2026-05-03 19:17:03 +0100

    Track only GitHub Actions and output folder

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTypeScript from "eslint-config-next/typescript";
-
-const eslintConfig = [
-  ...nextVitals,
-  ...nextTypeScript,
-];
-
-export default eslintConfig;
--- src/lib/hooks/useHydrated.ts
+++
@@ -1,7 +0,0 @@
-import { useSyncExternalStore } from 'react';
-
-const emptySubscribe = () => () => {};
-
-export function useHydrated(): boolean {
-  return useSyncExternalStore(emptySubscribe, () => true, () => false);
-}
